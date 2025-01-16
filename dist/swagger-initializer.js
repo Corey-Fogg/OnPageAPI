fd13a1febd50c3d79e6a0b0829cde2ac98ac7967
Corey Fogg <Corey.Fogg@outlook.com>
2025-01-16 09:52:41 -0500

    s

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -2,8 +2,8 @@ window.onload = function () {
   const ui = SwaggerUIBundle({
     dom_id: '#swagger-ui',
     urls: [
-      { name: 'OnPage Public API', url: 'OnPage Public API.yaml' },
-      { name: 'BlastIT Public API', url: 'BlastIT Public API.yaml' },
+      { name: 'OnPage Public API', url: 'specs\OnPage Public API.yaml' },
+      { name: 'BlastIT Public API', url: 'specs\BlastIT Public API.yaml' },
     ],
     deepLinking: true,
     presets: [
